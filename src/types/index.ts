e9051a7d06b94a412ffa58b83c56c0f6d33f2cfe
arashray <arash.es.1378@gmail.com>
2022-03-30 11:58:34 +0430

    fix build issues

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,11 @@
 // @ts-nocheck
-import { subscriberIdSymbol, subscribersSymbol } from "../utils";
+import { subscriberIdSymbol, subscribersSymbol } from '../utils';
 
-export type Subscriber<G = any> = { (data: G): void; [subscriberIdSymbol]: string };
+export type Subscriber<G = any> = {
+  (data: G): void;
+  [subscriberIdSymbol]: string;
+};
+
 export type NestedKeyOf<ObjectType extends object> = {
   [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
     ? ObjectType[Key] extends Array<any>
